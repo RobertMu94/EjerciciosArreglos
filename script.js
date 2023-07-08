0e33fdf16b22714bcf2bd7f008c0bdfa65b53e5b
unknown <robertmunoz@misena.edu.co>
2023-07-08 09:59:53 -0500

    ejerciiosfinal

---
+++ script.js
@@ -0,0 +1,34 @@
+
+/**Ejercicio 1 */
+
+/**let miarreglo = [];
+for (let i = 0; i < 10; i++){
+    miarreglo.push(Math.floor(Math.random() * 100));
+
+}
+console.log(miarreglo);
+
+*/
+
+/**Ejercicio 2 */
+
+/**const input = prompt("Ingrese una lista de números separados por coma");
+const array = input.split(",").map(Number);
+console.log(array);*/
+
+/**const input = prompt("Ingrese una lista de números separados por coma");
+const array = input.split(",");
+console.log(array);*/
+
+/**Ejercicio 3 */
+
+/**const arreglo = [ 10, 40, 30, 20, 15, 2, -7];
+arreglo.sort( (a, b) => a - b );
+var menor = arreglo[0];
+const mayor = arreglo[arreglo.length-1];
+
+
+alert("numeros de menor a mayor :" + arreglo + ".");
+alert("Numero menor : " + menor + ".");
+alert("Numero mayor : " + mayor + ".");*/
+
